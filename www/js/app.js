72544a4fbf92ee101722ba6c631dcb224fcde663
deutschlion <hello@itsmpl.ru>
2014-12-23 14:42:40 +0300

    Fat fixes

--- www/js/app.js
+++ www/js/app.js
@@ -22,25 +22,31 @@
       $data.selectedItem = selectedItem;
       $scope.ons.navigator.pushPage('detail.html', {title : selectedItem.title});
     };
+  });
 
-    $scope.scan = function(event) {
-        alert('scanning');
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-        scanner.scan( function (result) { 
-            alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
+  module.controller('ScanController', function($scope) {
 
-           document.getElementById("info").innerHTML = "Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n";
+      $scope.scanCode = function(event) {
+          $scope.ons.notification.alert({message: 'Scanning...'});
+          //
+          var scanner = $scope.cordova.require("cordova/plugin/BarcodeScanner");
+          $scope.ons.notification.alert({message: scanner});
+          //
+          scanner.scan( function (result) { 
+              alert("We got a barcode\n" + 
+              "Result: " + result.text + "\n" + 
+              "Format: " + result.format + "\n" + 
+              "Cancelled: " + result.cancelled);  
 
-        },function (error) { 
-            console.log("Scanning failed: ", error); 
-        });
-    };
+             document.getElementById("info").innerHTML = "Scanner result: \n" +
+                  "text: " + result.text + "\n" +
+                  "format: " + result.format + "\n" +
+                  "cancelled: " + result.cancelled + "\n";
+
+          },function (error) { 
+              console.log("Scanning failed: ", error); 
+          });
+      };
 
   });
 
@@ -73,82 +79,3 @@
       return data;
   });
 })();
-
-
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // `load`, `deviceready`, `offline`, and `online`.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.getElementById('scan').addEventListener('click', this.scan, false);
-        document.getElementById('encode').addEventListener('click', this.encode, false);
-    },
-
-    // deviceready Event Handler
-    //
-    // The scope of `this` is the event. In order to call the `receivedEvent`
-    // function, we must explicity call `app.receivedEvent(...);`
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    },
-
-    scan: function() {
-        console.log('scanning');
-        
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-
-        scanner.scan( function (result) { 
-
-            alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
-
-           console.log("Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
-            console.log(result);
-            /*
-            if (args.format == "QR_CODE") {
-                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-            }
-            */
-
-        }, function (error) { 
-            console.log("Scanning failed: ", error); 
-        } );
-    },
-
-    encode: function() {
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-
-        scanner.encode(scanner.Encode.TEXT_TYPE, "http://www.nhl.com", function(success) {
-            alert("encode success: " + success);
-          }, function(fail) {
-            alert("encoding failed: " + fail);
-          }
-        );
-
-    }
-
-};
